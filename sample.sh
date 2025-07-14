07159d103f4937f2c6998b1851d8e463105f36dc
hdmehta5 <hdmehta05@gmail.com>
2025-07-14 15:41:54 +0530

    First commit for main dir

---
+++ sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "This is a sample script."
+echo "Running some commands..."
+echo "Date and time:"
+echo $(date)
+echo "Enter a number to find the number is positive or negative: "
+read number
+if [ $number -gt 0 ]; then
+    echo "The number is positive."
+else    
+    echo "The number is negative."
+fi
+echo "All commands executed successfully."
